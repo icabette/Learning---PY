3943582d4d65f85fef24839a7f38217c591682bd
icabette <icabette@gmail.com>
2022-06-14 14:38:20 +0900

    Data structure Test

---
+++ myGlossary_Start.py
@@ -0,0 +1,29 @@
+# myGlossary_Start.py
+
+from tkinter import *
+
+# Key input function
+
+### main
+window = Tk()
+window.title("My Coding Club Glossary")
+
+# Label
+
+# Text entry widget
+
+# Add submit button
+
+# Other label
+
+# Create Text Box
+
+# Dictionary
+my_glossary = {
+    'Algorithm':'Computer logic to perform specific action',
+    'Bug':'Cause of unexpected action of program',
+    'Binary':'Binary number'
+}
+
+### loop main
+window.mainloop();
