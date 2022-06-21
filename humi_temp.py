4e3c61b3b522a3f7cdd953c15110310fa4ca3f99
icabette <icabette@gmail.com>
2022-06-21 16:07:30 +0900

    float update

--- humi_temp.py
+++ humi_temp.py
@@ -7,9 +7,10 @@ sensor = Adafruit_DHT.DHT11
 
 pin = 2
 
-humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+while(1):
+	humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
 
-if humidity is not None and temperature is not None:
-	print ('Temp={0:0.1f}*C Humidity={1:0.1f}%'.format(temperature, humidity))
-else:
-	print ('Failed to get reading. Try again!')
+	if humidity is not None and temperature is not None:
+		print ('Temp={0:0.4f}*C Humidity={1:0.4f}%'.format(temperature, humidity))
+	else:
+		print ('Failed to get reading. Try again!')
