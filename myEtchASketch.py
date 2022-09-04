f6467a0a5bfc948504dcb150b293d9ed77af04e1
icabette <icabette@gmail.com>
2022-09-04 21:49:22 +0900

    재환이랑 같이 함

--- myEtchASketch.py
+++ myEtchASketch.py
@@ -1,13 +1,14 @@
 from tkinter import *
 from turtle import bgcolor
+from time import sleep
 
-canvas_height = 400
-canvas_width = 600
-canvas_colour = "white"
+canvas_height = 800
+canvas_width = 1280
+canvas_colour = "black"
 
 p1_x = canvas_width/2
 p1_y = canvas_height
-p1_colour = "green"
+p1_colour = "red"
 line_width = 5
 line_length = 5
 
@@ -49,8 +50,18 @@ def erase_all(event):
 window = Tk()
 window.title("MyEtchASketch")
 canvas = Canvas(bg=canvas_colour, height=canvas_height, width=canvas_width, highlightthickness=0)
-
 canvas.pack()
+canvas.create_rectangle(200,50,400,250,fill="blue")
+canvas.create_oval(800,50,1000,250,fill="yellow")
+canvas.create_polygon(600,250,500,450,700,450,fill="orange")
+
+# while(1):
+#     canvas.create_oval(800,50,1000,250,fill="black")
+#     window.update()
+#     sleep(1)
+#     canvas.create_oval(800,50,1000,250,fill="yellow")
+#     window.update()
+#     sleep(1)
 
 # key reaction
 window.bind("<Up>",p1_move_N)
